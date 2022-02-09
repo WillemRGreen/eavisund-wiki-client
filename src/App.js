4a5c602d1481dcff6c54e72751856816c7981527
Will Green <willgreen@Wills-MacBook-Pro.local>
2022-02-09 15:35:49 -0600

    all updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from  'react'
+import { Routes, Route } from 'react-router-dom'
+import LandingPage from './LandingPage/LandingPage.js'
 
-function App() {
+class App extends Component {
+
+  renderRoutes() {
+    return (
+      <div>
+        <Routes>
+         <Route
+            path={'/'}
+            component={LandingPage}
+          />
+        </Routes>
+      </div>
+    )
+  }
+
+render(){
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        <header className="App-header">
+          Welcome to Eavisund
+        </header>
+      </div>
+      <main className='main'>
+        {this.renderRoutes()}
+      </main>
     </div>
+    
   );
+}
+  
 }
 
 export default App;
---
+++ src/NationsPage/NationsPage.js
@@ -0,0 +1,22 @@
+import './App.css';
+import React, { Component } from  'react'
+import { Link, Routes, Route } from 'react-router-dom'
+import GenericPage from '../GenericPage/GenericPage'
+
+
+const nationsPageTitle = <div>Nations of Eavisund</div>
+const nationsPageText = 
+<div>
+  <h3>The Known World</h3> 
+  <p>Currently, the known world is made up of a few continents, and many surrounding islands. There is Mödjord, the eastern continent, made up of three allied nations. There is the Illutan Empire, a vast continent to the west, surrounded by conquered island nations. And there is A’haqi, to the south of Mödjord, an ancient and mysterious land of desert nations, and the historic origin of the elves. An Introduction to Mödjord: Mödjord is made up of the “free people’s of Mödjord”: the human nation of Aegrund, the magocracy of Felas, and the mercantile nation of D’verthar.</p>
+</div>
+
+export default class InfoPage extends Component {
+
+    render(){
+        return (
+          undefined
+        );
+      }
+}
+
