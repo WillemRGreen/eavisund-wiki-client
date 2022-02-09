97cf8a29a3292e959818e6a9e0cfcd225693cd3f
Will Green <willgreen@Wills-MacBook-Pro.local>
2022-02-09 15:36:55 -0600

    final first day

--- src/NationsPage/NationsPage.js
+++ src/NationsPage/NationsPage.js
@@ -11,7 +11,7 @@ const nationsPageText =
   <p>Currently, the known world is made up of a few continents, and many surrounding islands. There is Mödjord, the eastern continent, made up of three allied nations. There is the Illutan Empire, a vast continent to the west, surrounded by conquered island nations. And there is A’haqi, to the south of Mödjord, an ancient and mysterious land of desert nations, and the historic origin of the elves. An Introduction to Mödjord: Mödjord is made up of the “free people’s of Mödjord”: the human nation of Aegrund, the magocracy of Felas, and the mercantile nation of D’verthar.</p>
 </div>
 
-export default class InfoPage extends Component {
+export default class NationsPage extends Component {
 
     render(){
         return (
